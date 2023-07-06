4849b95f70ad50dc30212aed55d930cbb9201d1b
philfan <1372597718@qq.com>
2023-07-06 18:21:26 +0800

    lec04

---
+++ lec04/demohello/src/hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: PhilFan
+	> Mail: Fanhuawei@zju.edu.cn 
+	> Created Time: 2023年07月06日 星期四 16时54分27秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+void hello(){
+	printf("Hello World!");
+	return ;
+}
