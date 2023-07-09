c254e0628ff082f8a3f8b1a0974b4dc37eebaa92
philfan <1372597718@qq.com>
2023-07-09 21:55:56 +0800

    proj

---
+++ proj/step-3/build/CMakeFiles/step-3.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/philfan/Projects/MathSoftware-2023/proj/step-3/step-3.cc" "CMakeFiles/step-3.dir/step-3.cc.o" "gcc" "CMakeFiles/step-3.dir/step-3.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ proj/step-3/build/CMakeFiles/step-3.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for step-3.
+# This may be replaced when dependencies are built.
